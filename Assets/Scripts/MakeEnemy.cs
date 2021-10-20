0952d27eae08437f3053f5a20e6c1512af88d5e9
keigo <b1951154@planet.kanazawa-it.ac.jp>
2021-10-20 15:37:13 +0900

    敵を生み出すオブジェクト生成

---
+++ Assets/Scripts/MakeEnemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeEnemy : MonoBehaviour
+{
+    public GameObject Player;
+    [SerializeField]
+    private Vector3 dictance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dictance = Player.transform.position - this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Vector3.Lerp(transform.position,
+            Player.transform.position - dictance, Time.deltaTime * 5);
+
+
+
+
+
+
+    }
+}
