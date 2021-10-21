3a0e853cf9550a2cd8b9ad698b6ef075dad12d51
keigo <b1951154@planet.kanazawa-it.ac.jp>
2021-10-21 22:26:11 +0900

    敵の自動生成

--- Assets/Scripts/MakeEnemy.cs
+++ Assets/Scripts/MakeEnemy.cs
@@ -2,28 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MakeEnemy : MonoBehaviour
 {
     public GameObject Player;
+    public GameObject MakeEnemyObject;
     [SerializeField]
-    private Vector3 dictance;
+    private Vector3 dictance;// Playerとの距離
+    public GameObject[] EnemyObject;// 敵を入れるリスト
+    [SerializeField]
+    private float EnemyTime; //敵が出てくる時間
+    private int number; //どの敵が出てくるかを決める数
+
+
+    // PlayerControllerのプレイヤーが動く距離を取得するため
+    public PlayerController playercontroller;
+    Vector3 moveZ = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
     {
+        //　プレイヤーとの距離
         dictance = Player.transform.position - this.transform.position;
+        EnemyTime = 3.0f;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = Vector3.Lerp(transform.position,
-            Player.transform.position - dictance, Time.deltaTime * 5);
 
+        // 値の取得
+        moveZ = playercontroller.MoveValue();
+        EnemyTime -= Time.deltaTime;
 
+        // Plyerとの距離を計算し、一定距離保つ
+        this.transform.position = Vector3.Lerp(transform.position, Player.transform.position - dictance, Time.deltaTime * 5);
+        
 
-
-
-
+        // 敵の射出とその時間
+        if (EnemyTime <= 0.0f)
+        {
+            EnemyTime = 1.0f;
+            number = Random.Range(0, EnemyObject.Length);
+            Instantiate(EnemyObject[number], new Vector3(0, Random.Range(1, 5), MakeEnemyObject.transform.position.z), Quaternion.identity);
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     CharacterController controller;
-    Vector3 movedir = Vector3.zero;
+    private Vector3 movedir = Vector3.zero;
 
-
+    // Playerの移動値を入れる変数
     public float speedX;
     public float speedZ;
     public float acceleratorZ;
 
+    // アニメーション
     private const string isRun = "isRunning";
     private const string isJump = "isJumping";
-
-
     Animator animator;
 
     // Start is called before the first frame update
@@ -28,14 +27,11 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
-
+    { 
 
         if (controller.isGrounded) {
             if (Input.GetKeyDown("space"))
             {
-
                 if (controller.isGrounded)
                 {
                     movedir.y = 10f;
@@ -49,6 +45,7 @@ public class PlayerController : MonoBehaviour
         
 
         movedir.z = Mathf.Clamp(movedir.z + (acceleratorZ * Time.deltaTime), 0, speedZ);
+        Debug.Log(movedir.z);
 
         if (movedir.z >= 0.1)
         {
@@ -64,6 +61,21 @@ public class PlayerController : MonoBehaviour
         if (controller.isGrounded)
         {
             movedir.y = 0;
+        }
+
+        Debug.Log(movedir.z);
+    }
+
+    public Vector3 MoveValue()
+    {
+        return movedir;
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.gameObject.tag == "Enemy")
+        { 
+            Destroy(hit.gameObject);
         }
     }
 }
